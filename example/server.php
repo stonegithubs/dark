58539a1f09f2bf002e805d6e5fdcde03177a2a36
dalu <guodalu@qq.com>
2017-01-16 23:32:32 +0800

    fix client

--- example/server.php
+++ example/server.php
@@ -4,6 +4,6 @@ require_once __DIR__. '/../src/Autoloader.php';
 
 $server = new \Dark\Server('127.0.0.1', 9501);
 $server->setDriver(function() {
-    return new \Dark\Driver\Pdo('mysql:host=localhost;dbname=ark', 'root', '');
+    return new \Dark\PdoDriver('mysql:host=localhost;dbname=ark', 'root', '');
 });
 $server->startup();
